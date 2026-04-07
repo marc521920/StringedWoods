62601ed1c41ba4c11dd2b72f4d5219adf8877dd6
Marc <marc.triguero.7e8@itb.cat>
2026-04-07 20:00:10 +0200

    movementBAsic

---
+++ Game/StringedWoods/Assets/Scripts/MainCharacterMovement.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class MainCharacterMovement : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    private Vector3 moveDirection = Vector3.zero;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+ 
+    void Update() {
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded) {
+            moveDirection = new Vector3(-Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+            
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
+ 
