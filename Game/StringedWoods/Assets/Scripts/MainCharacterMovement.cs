7f7d42a4fd8bbdfa6f4035263c1c56d250c3019b
Marc <marc.triguero.7e8@itb.cat>
2026-04-17 17:41:24 +0200

    ui

--- Game/StringedWoods/Assets/Scripts/MainCharacterMovement.cs
+++ Game/StringedWoods/Assets/Scripts/MainCharacterMovement.cs
@@ -289,6 +289,7 @@ public class MainCharacterMovement : MonoBehaviour
             canJump = true;
             // Aseguramos que la fuerza de golpe vuelva a su valor normal
             canAttack = false;
+            animator.SetBool("basicAttack", true);
              // Marcamos que estamos atacando
             attackArea.SetActive(true); // Activamos el área de ataque
             velocidadY = velocidadY/2f; // Detenemos el movimiento vertical durante el ataque
@@ -297,7 +298,7 @@ public class MainCharacterMovement : MonoBehaviour
 
             // Esperamos un momento para simular el tiempo de ataque
             yield return new WaitForSeconds(0.3f); // Ajusta este valor según la duración de tu animación
-            
+            animator.SetBool("basicAttack", false);
             attackArea.SetActive(false); // Desactivamos el área de ataque después de un momento
             isAttacking = false; // Terminamos el ataque
              // Desactivamos la posibilidad de atacar inmediatamente después
@@ -321,7 +322,7 @@ public class MainCharacterMovement : MonoBehaviour
         attackAreaJump.SetActive(true); // Activamos el área de ataque para el salto
 
         // Esperamos un momento para simular el tiempo de ataque
-        yield return new WaitForSeconds(0.2f); // Ajusta este valor según la duración de tu animación
+        yield return new WaitForSeconds(0.04f); // Ajusta este valor según la duración de tu animación
         attackAreaJump.SetActive(false); // Desactivamos el área de ataque después de un momento
         canMove = true; // Reactivamos el movimiento después del ataque
         canJump = true; // Reactivamos el salto después del ataque
